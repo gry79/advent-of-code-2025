031717548c7f72cdafb63c787d408545d89d83e9
Gry <gry79@users.noreply.github.com>
2025-12-07 14:28:59 +0100

    Added both Parts of Day 7

---
+++ src/test/java/ch/zkb/gry/aoc2025/day7/Day7Test.java
@@ -0,0 +1,32 @@
+package ch.zkb.gry.aoc2025.day7;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day7Test {
+
+    @Test
+    void part1() {
+        try (FileInputStream fis = new FileInputStream("./src/test/resources/day7/input.txt")) {
+            long count = Day7Part1.compute(fis);
+            assertEquals(1642, count);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void part2() {
+        try (FileInputStream fis = new FileInputStream("./src/test/resources/day7/input.txt")) {
+            long count = Day7Part2.compute(fis);
+            assertEquals(47274292756692L, count);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
